2501c2c3f8858bb73dab4630230e0441278d35ea
Umutoniwase Dorcas <uwasedorcas22@gmail.com>
2025-04-25 10:19:48 -1000

    first commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>umutoniwase dorcas PHP</title>
+</head>
+<body>
+    <h1>Umutoniwase dorcas PHP</h1>
+
+    <pre>
+    U   U
+    U   U
+    U   U
+    U   U
+    UUUUU
+    </pre>
+
+    <p>
+        SHA256 hash of "umutoniwase dorcas":<br>
+        <?php
+            print hash('sha256', 'Umutoniwase dorcass');
+        ?>
+    </p>
+</body>
+</html>
